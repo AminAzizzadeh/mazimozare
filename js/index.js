134a833daebbbcb59156c888123253580ffa8fde
AminAzizzadeh <aminazizzadeh13@gmail.com>
2022-05-25 03:44:54 +0430

    v0.2
    Original By Amin Azizzadeh

--- js/index.js
+++ js/index.js
@@ -197,21 +197,36 @@ document.querySelector("body").addEventListener("click", function(){
 })
 
 document.querySelector(".randomize").addEventListener("click", function(){
+    result.textContent = "";
+
     randomizer1 = Math.floor(Math.random() * 3) + 1;
     randomizer2 = Math.floor(Math.random() * 2) + 1;
     randomizer3 = Math.floor(Math.random() * 3) + 1;
     randomizer4 = Math.floor(Math.random() * 2) + 1;
 
+    if(randomizer1 == 3 && randomizer3 == 1){
+        randomizer3 = Math.floor(Math.random() * 3) + 2;
+    }
+
     if(randomizer1 == 1){
         s[0].value = o[1].value;
+        change[0].classList.remove("hidden");
+        change[1].textContent = "مثني";
+        change[2].textContent = "جمع"; 
     }
     
     if(randomizer1 == 2){
         s[0].value = o[2].value;
+        change[0].classList.remove("hidden");
+        change[1].textContent = "مثني";
+        change[2].textContent = "جمع"; 
     }
 
     if(randomizer1 == 3){
         s[0].value = o[3].value;
+        change[0].classList.add("hidden");
+        change[1].textContent = "وحده";
+        change[2].textContent = "مع الغير";
     }
     
     if(randomizer2 == 1){
@@ -221,16 +236,16 @@ document.querySelector(".randomize").addEventListener("click", function(){
     if(randomizer2 == 2){
         s[1].value = o[6].value;
     }
-
-    if(randomizer3 == 3){
-        s[2].value = o[8].value;
-    }
     
     if(randomizer3 == 1){
-        s[2].value = o[9].value;
+        s[2].value = o[8].value;
     }
 
     if(randomizer3 == 2){
+        s[2].value = o[9].value;
+    }
+
+    if(randomizer3 == 3){
         s[2].value = o[10].value;
     }
     
